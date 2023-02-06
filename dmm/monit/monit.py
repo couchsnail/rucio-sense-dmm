fc162d7550f0f48a664a960a195a5ba3d4c6dcdd
aaarora <aaarora@ucsd.edu>
2023-02-06 12:30:06 -0800

    update

--- dmm/monit/monit.py
+++ dmm/monit/monit.py
@@ -13,13 +13,14 @@ class MonitConfig():
             prometheus_config = yaml.safe_load(f_in).get("prometheus")
             prometheus_host = prometheus_config["host"]
             prometheus_port = prometheus_config["port"]
-            ftsmonit_config = yaml.safe_load(f_in).get("ftsmonit")
-            ftsmonit_host = ftsmonit_config["host"]
-            ftsmonit_port = ftsmonit_config["port"]
+            # ftsmonit_config = yaml.safe_load(f_in).get("ftsmonit")
+            # ftsmonit_host = ftsmonit_config["host"]
+            # ftsmonit_port = ftsmonit_config["port"]
         self.prometheus_addr = f"http://{prometheus_host}:{prometheus_port}"
-        self.ftsmonit_addr = f"http://{ftsmonit_host}:{ftsmonit_port}"
+        # self.ftsmonit_addr = f"http://{ftsmonit_host}:{ftsmonit_port}"
 
-def submit_prom_query(config, query_dict, endpoint="api/v1/query") -> dict:
+def submit_prom_query(config, query_dict) -> dict:
+    endpoint = "api/v1/query"
     query_addr = f"{config.prometheus_addr}/{endpoint}"
     return requests.get(query_addr, params=query_dict).json()
 
@@ -34,17 +35,18 @@ def get_interface(config, ipv6) -> str:
             if metric["metric"]["address"] == ipv6:
                 return [metric["metric"]["device"], metric["metric"]["instance"]]
 
-def get_total_bytes_at_t(time) -> float:
+def get_total_bytes_at_t(time, device, instance, rse_name) -> float:
     """
     Returns the total number of bytes transmitted from a given Rucio RSE via a given
     ipv6 address
     """
-    params = f"device=\"{self.device_info[0]}\",instance=\"{self.device_info[1]}\",job=~\".*{self.rse_name}.*\""
+    # TODO device_info
+    params = f"device=\"{device}\",instance=\"{instance}\",job=~\".*{rse_name}.*\""
     metric = f"node_network_transmit_bytes_total{{{params}}}"
     # Get bytes transferred at the start time
-    response = self.submit_query(self.prometheus_addr, {"query": metric, "time": time})
+    response = submit_query(config.prometheus_addr, {"query": metric, "time": time})
     if response["status"] == "success":
-        bytes_at_t = self.get_val_from_response(response)
+        bytes_at_t = get_val_from_response(response)
     else:
         raise Exception(f"query {metric} failed")
     return float(bytes_at_t)
--- dmm/monit/server.py
+++ dmm/monit/server.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from monit import get_throughput_at_t
+from os import environ
+from time import time
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+m = MonitConfig(configfile=environ.get("MONIT_CONFIG"))
+
+@app.post("/query")
+def get_total_bytes(ipv6: str, rse_name: str):
+    dev, int = get_interface(endpoint_1)
+    return get_total_bytes_at_t(time(), dev, instance, rse_name)    
