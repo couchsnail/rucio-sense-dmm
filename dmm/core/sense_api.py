30e2e5a866632aff757634cd27ee393bb68c7325
aaarora <aaarora@ucsd.edu>
2023-09-11 12:58:56 -0700

    refactored

--- dmm/core/sense_api.py
+++ dmm/core/sense_api.py
@@ -1,10 +1,10 @@
 import json
-from dmm.utils.config import config_get
 import re
 import logging
 
+from dmm.utils.config import config_get
+
 from sense.client.workflow_combined_api import WorkflowCombinedApi
-from sense.client.profile_api import ProfileApi
 from sense.client.discover_api import DiscoverApi
 
 PROFILE_UUID = ""
@@ -41,148 +41,98 @@ def get_uplink_capacity(uri):
         #return float(response["peer_points"][0]["port_capacity"])
         return 10000
 
+def good_response(response):
+    return bool(response and not any("ERROR" in r for r in response))
+
 def get_uri(rse_name, regex=".*?", full=False):
-    """Return the SENSE URI for a given Rucio RSE"""
     discover_api = DiscoverApi()
     response = discover_api.discover_lookup_name_get(rse_name, search="NetworkAddress")
-    logging.debug(response)
     if not good_response(response):
         raise ValueError(f"Discover query failed for {rse_name}")
-    else:
-        response = json.loads(response)
-        if not response["results"]:
-            raise ValueError(f"No results for '{rse_name}'")
-        matched_results = []
-        for result in response["results"]:
-            if re.search(regex, result["name/tag/value"]):
-                matched_results.append(result)
-        if len(matched_results) == 0:
-            raise ValueError(f"No results matched '{regex}'")
-        else:
-            full_uri = matched_results[0]["resource"]
-            if full:
-                return full_uri
-            else:
-                return __get_rooturi(full_uri)
-
-def __get_rooturi(full_uri):
-    """Return the root SENSE URI for a given full SENSE URI"""
-    discover_api = DiscoverApi()
+    response = json.loads(response)
+    if not response["results"]:
+        raise ValueError(f"No results for '{rse_name}'")
+    matched_results = [result for result in response["results"] if re.search(regex, result["name/tag/value"])]
+    if len(matched_results) == 0:
+        raise ValueError(f"No results matched '{regex}'")
+    full_uri = matched_results[0]["resource"]
+    if full:
+        return full_uri
     root_uri = discover_api.discover_lookup_rooturi_get(full_uri)
     if not good_response(root_uri):
         raise ValueError(f"Discover query failed for {full_uri}")
-    else:
-        return root_uri
+    return root_uri
 
 def stage_link(src_uri, dst_uri, src_ipv6, dst_ipv6, instance_uuid="", alias=""):
-    """Return the maximum theoretical bandwidth available between two sites
-
-    Note: not fully supported by SENSE yet
-    """
     workflow_api = WorkflowCombinedApi()
-    if instance_uuid == "":
-        workflow_api.instance_new()
-    else:
-        workflow_api.si_uuid = instance_uuid
-    # Create service instance
+    workflow_api.instance_new() if instance_uuid == "" else setattr(workflow_api, "si_uuid", instance_uuid)
     intent = {
-        "service_profile_uuid": get_profile_uuid(),
+        "service_profile_uuid": PROFILE_UUID,
         "queries": [
             {
-                "ask": "edit", 
+                "ask": "edit",
                 "options": [
-                    # Source
-                    {"data.connections[0].terminals[0].uri": src_uri},
-                    {"data.connections[0].terminals[0].ipv6_prefix_list": src_ipv6},
-                    # Destination
-                    {"data.connections[0].terminals[1].uri": dst_uri},
-                    {"data.connections[0].terminals[1].ipv6_prefix_list": dst_ipv6},
+                    {"data.connections[0].terminals[0].uri": src_uri}, {"data.connections[0].terminals[0].ipv6_prefix_list": src_ipv6},
+                    {"data.connections[0].terminals[1].uri": dst_uri}, {"data.connections[0].terminals[1].ipv6_prefix_list": dst_ipv6},
                 ]
             },
-            {
-                "ask": "maximum-bandwidth", 
-                "options": [{"name": "Connection 1"}]
-            }
+            {"ask": "maximum-bandwidth", "options": [{"name": "Connection 1"}]}
         ]
     }
     if alias:
         intent["alias"] = alias
-    # Query SENSE and extract theoretical bandwidth from its response
     response = workflow_api.instance_create(json.dumps(intent))
-    logging.debug(response)
     if not good_response(response):
         raise ValueError(f"SENSE query failed for {instance_uuid}")
-    else:
-        response = json.loads(response)
-        for query in response["queries"]:
-            if query["asked"] == "maximum-bandwidth":
-                result = query["results"][0]
-                if "bandwidth" not in result:
-                    raise ValueError(f"SENSE query failed for {instance_uuid}")
-                else:
-                    return response["service_uuid"], float(result["bandwidth"])
+    response = json.loads(response)
+    for query in response["queries"]:
+        if query["asked"] == "maximum-bandwidth":
+            result = query["results"][0]
+            if "bandwidth" not in result:
+                raise ValueError(f"SENSE query failed for {instance_uuid}")
+            return response["service_uuid"], float(result["bandwidth"])
 
 def provision_link(instance_uuid, src_uri, dst_uri, src_ipv6, dst_ipv6, bandwidth, alias=""):
-    """Create a SENSE guaranteed-bandwidth link between two sites"""
     workflow_api = WorkflowCombinedApi()
     workflow_api.si_uuid = instance_uuid
-    # Modify service instance
-    logging.debug(f"instance uuid: {instance_uuid}")
     intent = {
-        "service_profile_uuid": get_profile_uuid(),
+        "service_profile_uuid": PROFILE_UUID,
         "queries": [
             {
-                "ask": "edit", 
+                "ask": "edit",
                 "options": [
-                    # Bandwidth
                     {"data.connections[0].bandwidth.capacity": str(bandwidth)},
-                    # Source
-                    {"data.connections[0].terminals[0].uri": src_uri},
-                    {"data.connections[0].terminals[0].ipv6_prefix_list": src_ipv6},
-                    # Destination
-                    {"data.connections[0].terminals[1].uri": dst_uri},
-                    {"data.connections[0].terminals[1].ipv6_prefix_list": dst_ipv6},
+                    {"data.connections[0].terminals[0].uri": src_uri}, {"data.connections[0].terminals[0].ipv6_prefix_list": src_ipv6},
+                    {"data.connections[0].terminals[1].uri": dst_uri}, {"data.connections[0].terminals[1].ipv6_prefix_list": dst_ipv6},
                 ]
             }
         ]
     }
     if alias:
         intent["alias"] = alias
-    # Push intent JSON to SENSE
     response = workflow_api.instance_create(json.dumps(intent))
-    logging.debug(response)
     if not good_response(response):
         raise ValueError(f"SENSE query failed for {instance_uuid}")
-    else:
-        response = json.loads(response)
-        workflow_api.instance_operate("provision", sync="true")
+    response = json.loads(response)
+    workflow_api.instance_operate("provision", sync="true")
 
 def delete_link(instance_uuid):
-    """Delete a SENSE link"""
     workflow_api = WorkflowCombinedApi()
     status = workflow_api.instance_get_status(si_uuid=instance_uuid)
-    logging.debug(status)
     if "error" in status:
         raise ValueError(status)
-    if "CREATE" not in status and "REINSTATE" not in status and "MODIFY" not in status:
-        raise ValueError(f"cannot cancel an instance in status '{status}'")
-    workflow_api.instance_operate(
-        "cancel", 
-        si_uuid=instance_uuid, 
-        sync="true", 
-        force=str("READY" not in status).lower()
-    )
+    if not any(status.startswith(s) for s in ["CREATE", "REINSTATE", "MODIFY"]):
+        raise ValueError(f"Cannot cancel an instance in status '{status}'")
+    workflow_api.instance_operate("cancel", si_uuid=instance_uuid, sync="true", force=str("READY" not in status).lower())
     status = workflow_api.instance_get_status(si_uuid=instance_uuid)
-    logging.debug(status)
     if "CANCEL - READY" in status:
         workflow_api.instance_delete(si_uuid=instance_uuid)
     else:
-        raise Exception(f"cancel operation disrupted; instance not deleted")
+        raise Exception(f"Cancel operation disrupted; instance not deleted")
 
 def reprovision_link(old_instance_uuid, src_uri, dst_uri, src_ipv6, dst_ipv6, 
                      new_bandwidth, alias=""):
     """Reprovision a SENSE link
-
     Note: this currently deletes the existing link, then creates a copy of the old link
           with the new bandwidth provision; this is NOT how it will ultimately be done 
           in production, but an actual reprovisioning is not currently supported
