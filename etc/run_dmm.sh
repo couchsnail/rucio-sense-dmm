acf3f336d2844bde9a11553e33b25452bc0a6244
Camille Sicat <cisicat@ucsd.edu>
2024-07-23 09:44:33 -0700

    Added files

---
+++ etc/run_dmm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Starting DMM development environment"
+docker build . -t dmm-debug 
+
+# if --rm passed then trap docker rm postgres
+trap "docker stop postgres" EXIT
+
+echo "Starting Postgres"
+docker run -d --rm \
+--network host \
+--name postgres \
+-e POSTGRES_USER=dmm \
+-e POSTGRES_PASSWORD=dmm \
+postgres
+
+echo "Starting DMM"
+docker run -it --rm \
+--network host \
+--add-host nrp-01.nrp-nautilus.io:127.0.0.1 \
+-v $HOME/dmm.cfg:/opt/dmm/dmm.cfg \
+-v $HOME/rucio.cfg:/opt/rucio/etc/rucio.cfg \
+-v $HOME/.sense-o-auth.yaml:/root/.sense-o-auth.yaml \
+-v /etc/grid-security/certificates/:/etc/grid-security/certificates \
+--name dmm-cisicat \
+dmm-debug
